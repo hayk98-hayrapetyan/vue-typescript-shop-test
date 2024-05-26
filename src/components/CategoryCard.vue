3f47bc9d3cddc153451203eeacd1300a6fea3102
hayk98-hayrapetyan <hayk.hayrapetyan98@gmail.com>
2024-05-26 18:59:04 +0400

    Create Category card component

---
+++ src/components/CategoryCard.vue
@@ -0,0 +1,15 @@
+<template>
+    <div class="flex items-center bg-white rounded-lg border px-4 py-2">
+      <img class="w-12 h-12 rounded-full object-cover mr-4" :src="category.thumbnailUrl" alt="Category image">
+      <div class="text-lg font-semibold text-gray-900">{{ category.name }}</div>
+    </div>
+</template>
+  
+<script setup lang="ts">
+import type { Category } from '@/types';
+
+defineProps<{
+    category: Category
+}>();
+</script>
+  
